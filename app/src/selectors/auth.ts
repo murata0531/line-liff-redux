caae573221e1c5901690864cadb296c18deabce9
murata0531 <coless115@gmail.com>
2021-09-29 23:43:05 +0900

    Redux Store から欲しいデータを取得するための Selector

---
+++ app/src/selectors/auth.ts
@@ -0,0 +1,37 @@
+import {createSelector} from "reselect";
+import {RootState} from "../store";
+
+// auth情報を取得するSelector
+export const authSelector = (state: RootState) => state.auth;
+
+/**
+ * liffIdTokenを取得する
+ * @returns liffIdToken
+ */
+export const liffIdTokenSelector = createSelector(authSelector, (auth) => {
+    return auth.liffIdToken;
+});
+
+/**
+ * LINEの名前を取得する
+ * @returns displayName
+ */
+export const displayNameSelector = createSelector(authSelector, (auth) => {
+    return auth.displayName;
+});
+
+/**
+ * LINEの画像を取得する
+ * @returns pictureUrl
+ */
+export const pictureUrlSelector = createSelector(authSelector, (auth) => {
+    return auth.pictureUrl;
+});
+
+/**
+ * errorを取得する
+ * @returns error
+ */
+export const errorSelector = createSelector(authSelector, (auth) => {
+    return auth.error;
+});
